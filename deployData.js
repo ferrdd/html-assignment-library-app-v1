f704988d5ce2a8b86aae73324efb3b532c169a43
ferrdd <ferditok1811@gmail.com>
2024-10-17 13:35:20 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Mohammad Ferdiansah Rahmadani'; // TODO: replace this
+
+const studentId = 'FS11564951'; // TODO: replace this
+
+const NetlifyDeployUrl = 'https://spontaneous-conkies-9ef9de.netlify.app'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
